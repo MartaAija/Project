ac6021d35fa9d19cfdf3eaa14f52a6409f842394
Marta Zigure <121212689+MartaAija@users.noreply.github.com>
2025-02-19 19:30:12 +0000

    Use Railway PostgreSQL database

--- backend/database.py
+++ backend/database.py
@@ -1,12 +1,13 @@
+import os
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
 # Database URL (ensure the password is correct)
-DATABASE_URL = "postgresql://postgres:Rainbows?13@localhost:5433/UserDB"
+DATABASE_URL = os.getenv("DATABASE_URL")
 
 # Create an engine to connect to PostgreSQL
-engine = create_engine(DATABASE_URL, echo=True)
+engine = create_engine(DATABASE_URL)
 
 # Base class for declarative models
 Base = declarative_base()
